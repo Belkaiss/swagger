af8fbf25b4bf864d5ff2a9b141d90d0077879438
belkaiss <belkiskhadija@gmail.com>
2022-12-08 16:53:27 +0100

    create project swagger

---
+++ src/main/java/com/exp/DemoSwagApplication.java
@@ -0,0 +1,13 @@
+package com.exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSwagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSwagApplication.class, args);
+    }
+
+}
